23b4ad958db5c808e0ad2b69ed323313f6d5a518
Bartosz <kwiatkowsky.kontakt@gmail.com>
2022-04-10 17:01:03 +0200

    Finish basic usefull unittests.

--- test/LinkedList-test.cpp
+++ test/LinkedList-test.cpp
@@ -1,4 +1,32 @@
-//
-// Created by bartosz on 2022-04-10.
-//
+#include "gtest/gtest.h"
+#include "LinkedList.h"
 
+TEST(LinkedListTest, constructor)
+{
+  LinkedList<int> list(13, 0);
+  EXPECT_EQ(list.get_size(), 1);
+}
+
+TEST(LinkedListTest, get_data_at_index)
+{
+  LinkedList<int> list(13, 0);
+  EXPECT_EQ(list[0], 13);
+}
+
+TEST(LinkedListTest, soring)
+{
+  LinkedList<double> list(3.3, 2);
+  list.add(9.9, 0);
+  list.add(2.0, 1);
+  EXPECT_EQ(list[0], 9.9);
+  EXPECT_EQ(list[1], 2.0);
+  EXPECT_EQ(list[2], 3.3);
+}
+
+TEST(LinkedListTest, size)
+{
+  LinkedList<char> list('a', 0);
+  EXPECT_EQ(list.get_size(), 1);
+  list.add('c', -1);
+  EXPECT_EQ(list.get_size(), 2);
+}
--- test/Node-test.cpp
+++ test/Node-test.cpp
@@ -1,4 +1,27 @@
-//
-// Created by bartosz on 2022-04-10.
-//
+#include "gtest/gtest.h"
+#include "Node.h"
 
+TEST (NodeTest, constructor)
+{
+  Node<int> node(13, 0);
+  EXPECT_EQ(node.get_data(), 13);
+  EXPECT_EQ(node.get_priority(), 0);
+  EXPECT_EQ(node.get_next(), nullptr);
+}
+
+TEST(NodeTst, getter_and_setter_data)
+{
+  Node<std::string> node("Hello", 1);
+  EXPECT_EQ(node.get_data(), "Hello");
+  node.set_data("Test");
+  EXPECT_EQ(node.get_data(), "Test");
+}
+
+TEST(NodeTst, getter_and_setter_next)
+{
+  Node<double> *first = new Node<double>(7.6, 0);
+  Node<double> *secound= new Node<double>(0.2, 1);
+  first->set_next(secound);
+
+  EXPECT_EQ(first->get_next(), secound);
+}
